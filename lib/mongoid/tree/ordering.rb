d517dd8b074646445195a6d0df8e2c53b8971c69
endy <endy1122@gmail.com>
2014-04-11 14:56:32 +0900

    ordering file change for debuging

--- lib/mongoid/tree/ordering.rb
+++ lib/mongoid/tree/ordering.rb
@@ -1,36 +1,36 @@
-module Mongoid
-  module Tree
-    ##
-    # = Mongoid::Tree::Ordering
-    #
-    # Mongoid::Tree doesn't order the tree by default. To enable ordering of children
-    # include both Mongoid::Tree and Mongoid::Tree::Ordering into your document.
-    #
-    # == Utility methods
-    #
-    # This module adds methods to get related siblings depending on their position:
-    #
-    #    node.lower_siblings
-    #    node.higher_siblings
-    #    node.first_sibling_in_list
-    #    node.last_sibling_in_list
-    #
-    # There are several methods to move nodes around in the list:
-    #
-    #    node.move_up
-    #    node.move_down
-    #    node.move_to_top
-    #    node.move_to_bottom
-    #    node.move_above(other)
-    #    node.move_below(other)
-    #
-    # Additionally there are some methods to check aspects of the document
-    # in the list of children:
-    #
-    #    node.at_top?
-    #    node.at_bottom?
-    module Ordering
-      # extend ActiveSupport::Concern
+# module Mongoid
+#   module Tree
+#     ##
+#     # = Mongoid::Tree::Ordering
+#     #
+#     # Mongoid::Tree doesn't order the tree by default. To enable ordering of children
+#     # include both Mongoid::Tree and Mongoid::Tree::Ordering into your document.
+#     #
+#     # == Utility methods
+#     #
+#     # This module adds methods to get related siblings depending on their position:
+#     #
+#     #    node.lower_siblings
+#     #    node.higher_siblings
+#     #    node.first_sibling_in_list
+#     #    node.last_sibling_in_list
+#     #
+#     # There are several methods to move nodes around in the list:
+#     #
+#     #    node.move_up
+#     #    node.move_down
+#     #    node.move_to_top
+#     #    node.move_to_bottom
+#     #    node.move_above(other)
+#     #    node.move_below(other)
+#     #
+#     # Additionally there are some methods to check aspects of the document
+#     # in the list of children:
+#     #
+#     #    node.at_top?
+#     #    node.at_bottom?
+#     module Ordering
+#       extend ActiveSupport::Concern
 # 
 #       included do
 #         field :position, :type => Integer
@@ -231,6 +231,6 @@ module Mongoid
 #       def assign_default_position?
 #         self.position.nil? || self.parent_id_changed?
 #       end
-    end
-  end
-end
+#     end
+#   end
+# end
